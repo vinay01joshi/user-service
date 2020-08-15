cbdc75d1b111b8c732ac94d04a5bddffb7ac0223
Vinay Joshi <vinay01joshi@gmail.com>
2020-08-15 14:49:24 +0530

    Adding User Model

---
+++ src/main/java/com/vinjo/spring/userservice/model/User.java
@@ -0,0 +1,54 @@
+package com.vinjo.spring.userservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id ;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User() {
+        super();
+    }
+
+}
